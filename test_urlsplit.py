d1bd136cc5a055a7f581662fc5f2d10f745bee01
SMWARREN <sean.warren@gmail.com>
2015-05-02 16:22:19 -0400

    Updated Python Files

---
+++ test_urlsplit.py
@@ -0,0 +1,40 @@
+from unittest import TestCase
+from urlsplit2 import Urlsplit
+__author__ = 'SWarren'
+
+
+class TestUrlsplit(TestCase):
+    def test_error_no_HTTP_HTTPS(self):
+        HttpTest = Urlsplit().SplitIntoList('www.makeit.org/demo/appl?input=3')
+        self.assertEqual(HttpTest,'error in input',msg='not equal')
+        print('Testing the address www.makeit.org/demo/appl?input=3')
+        print('Error No HTTP:// or HTTPS:// Test Sucessful')
+        print('')
+
+    def test_Correct_Address(self):
+        HttpTest = Urlsplit().SplitIntoList('http://www.makeit.org/demo/appl?input=3')
+        actualTest = ['http', 'www.makeit.org', '/demo/appl', 'input=3']
+        self.assertEqual(HttpTest,actualTest,msg='not equal')
+
+        print('Testing the address http://www.makeit.org/demo/appl?input=3')
+        print('Correct Address Test Successful')
+        print('')
+
+    def test_Garabage(self):
+        HttpTest = Urlsplit().SplitIntoList('fuckyourcouch.com/bitch')
+        '''actualTest = ['http', 'www.makeit.org', '/demo/appl', 'input=3']'''
+        print(HttpTest)
+
+        self.assertEqual(HttpTest,'error in input',msg='not equal')
+
+        print('Garabage Test Successful')
+        print('')
+
+
+
+
+    def test_new(self):
+
+        print('The Tests have been Completed')
+
+
---
+++ urlsplit2.py
@@ -0,0 +1,17 @@
+__author__ = 'SWarren'
+from urllib.parse import urlparse
+
+class Urlsplit:
+    def SplitIntoList(self,address):
+        o = urlparse(address)
+        if o.scheme or o.netloc != '':
+            urlList = [o.scheme,o.netloc,o.path,o.query]
+            return urlList
+        else:
+            return 'error in input'
+
+print('%s' % Urlsplit().SplitIntoList('http://www.makeit.org/demo/appl?input=3'))
+
+print('%s' % Urlsplit().SplitIntoList('http://.iloveyou.com/whatsup/hey.php?chicken=1'))
+
+
